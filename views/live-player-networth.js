0372bda0768847d9e779f2c0b2be5f80c4e6a7d4
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2022-09-23 13:47:08 +0900

    feat: to get player information

--- views/live-player-networth.js
+++ views/live-player-networth.js
@@ -41,12 +41,6 @@ module.exports = ({ selectedMatchId } = {}) => {
         }))
       );
 
-    if (allPlayers.some(({ account_id }) => !(account_id in playerInfos))) {
-      allPlayers.forEach(({ account_id }) => {
-        if (!(account_id in playerInfos)) fetchPlayerInfo(account_id);
-      });
-    }
-
     const sortedByNetWorth = allPlayers
       .sort((a, b) => b.net_worth - a.net_worth)
       .map(({ account_id, hero_id, net_worth, side }) => ({
@@ -65,13 +59,52 @@ module.exports = ({ selectedMatchId } = {}) => {
     setSortedPlayerNetworths(sortedByNetWorth);
   }, [selectedMatchId, matches, heroes, playerInfos]);
 
+  useEffect(() => {
+    if (!selectedMatchId || !matches.length) return;
+
+    const selectedMatch = matches.find(
+      (match) => match.match_id === selectedMatchId
+    );
+
+    const { scoreboard } = selectedMatch || {};
+    const { radiant, dire } = scoreboard || {};
+    const { players: rPlayers } = radiant || {};
+    const { players: dPlayers } = dire || {};
+
+    if (!rPlayers || !dPlayers) return console.log("No players found");
+
+    const allPlayers = rPlayers
+      .map(({ ...rest }) => ({
+        ...rest,
+        side: "radiant",
+      }))
+      .concat(
+        dPlayers.map(({ ...rest }) => ({
+          ...rest,
+          side: "dire",
+        }))
+      );
+
+    fetchPlayersInfo(allPlayers, playerInfos);
+
+    async function fetchPlayersInfo(allPlayers) {
+      for await (const player of allPlayers) {
+        if (!(player.account_id in playerInfos))
+          await fetchPlayerInfo(player.account_id);
+      }
+    }
+  }, [selectedMatchId, matches]);
+
   return (
     <Box flexDirection="column">
       {sortedPlayerNetworths.map(
         ({ account_id, player_name, hero_name, net_worth, side }) => (
           <Text key={account_id + hero_name}>
             [<Text>{side.slice(0, 1).toUpperCase()}</Text>] {net_worth} ||{" "}
-            {hero_name} || {playerInfos[account_id]?.Name || player_name}
+            {hero_name} ||{" "}
+            {playerInfos[account_id]?.Name
+              ? "*" + playerInfos[account_id]?.Name
+              : player_name}
           </Text>
         )
       )}
