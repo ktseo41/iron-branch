3523020b78dfca00419882f12785fe336e4963ae
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2022-09-24 16:45:03 +0900

    feat: show duration on match selector

--- views/match-selector.js
+++ views/match-selector.js
@@ -3,28 +3,44 @@ const React = require("react");
 const { Text } = require("ink");
 const Prompts = require("import-jsx")("../components/prompts.js");
 const { getGameStateFromMatch } = require("../lib/match");
+const { GAME_STATE } = require("../constants");
 
 function extractTeamsFromGames(games) {
   const gameSummaries = games
     .filter(({ radiant_team, dire_team }) => radiant_team && dire_team)
     .map((game) => {
-      return {
+      const gameState = getGameStateFromMatch(game);
+
+      const summary = {
         id: game.match_id,
         radiantTeam: game.radiant_team?.team_name,
         direTeam: game.dire_team?.team_name,
-        gameState: getGameStateFromMatch(game),
+        gameState,
       };
+
+      if (gameState === GAME_STATE.IN_GAME) {
+        summary.parsedDuration =
+          String(Math.floor(game.scoreboard?.duration / 60)).padStart(2, "0") +
+          ":" +
+          String(Math.floor(game.scoreboard?.duration % 60)).padStart(2, "0");
+      }
+
+      return summary;
     });
 
-  return gameSummaries.map(({ id, radiantTeam, direTeam, gameState }) => ({
-    id,
-    label: `${radiantTeam} vs ${direTeam} (${gameState})`,
-    value: {
-      radiantTeam,
-      direTeam,
+  return gameSummaries.map(
+    ({ id, radiantTeam, direTeam, gameState, parsedDuration }) => ({
       id,
-    },
-  }));
+      label: `${radiantTeam} vs ${direTeam} (${gameState}${
+        parsedDuration ? " " + parsedDuration : ""
+      })`,
+      value: {
+        radiantTeam,
+        direTeam,
+        id,
+      },
+    })
+  );
 }
 
 module.exports = ({ matches = [], onSelected }) => {
