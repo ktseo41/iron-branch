36142a442614a6b98aedd56108939cfe73d6bee2
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2022-10-08 14:58:40 +0900

    feat: refactor Scoreboard to startz API

--- views/MatchDetails.jsx
+++ views/MatchDetails.jsx
@@ -3,13 +3,14 @@ import useLiveCurrentMatch from "../hooks/useLiveCurrentMatch";
 import { GAME_STATE } from "../constants";
 import LiveBanpickPhase from "./LiveBanpickPhase";
 import LivePlayerNetworth from "./LivePlayerNetworth";
+import Scoreboard from "./Scoreboard";
 
 export default function MatchDetails() {
   const { currentMatch } = useLiveCurrentMatch();
 
   return (
     <>
-      {/* <Scoreboard /> */}
+      <Scoreboard />
       { !currentMatch ? (<Text>loading...</Text>) : (
         <>
           {currentMatch.gameState !== GAME_STATE.GAME_IN_PROGRESS && <LiveBanpickPhase />}
--- views/Scoreboard.jsx
+++ views/Scoreboard.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 import { Text } from "ink";
-import useLiveMatches from "../hooks/useLiveMatches";
-import useGameState from "../hooks/useGameState";
-import { GAME_STATE } from "../constants";
 import useCleanUp from "../hooks/useCleanUp";
+import useLiveCurrentMatch from "../hooks/useLiveCurrentMatch";
+import { GAME_STATE } from "../constants";
 
-// eslint-disable-next-line react/prop-types
-export default function Scoreboard({ selectedMatchId } = {}) {
-  const { matches } = useLiveMatches({ useInterval: true, from: "scoreboard" });
+export default function Scoreboard() {
+  const { currentMatch } = useLiveCurrentMatch();
   const [radiantTeamName, setRadiantTeamName] = useState();
   const [direTeamName, setDireTeamName] = useState();
   const [radiantNetworthSum, setRadiantNetworthSum] = useState();
@@ -15,65 +13,46 @@ export default function Scoreboard({ selectedMatchId } = {}) {
   const [radiantKillScores, setRadiantKillScores] = useState();
   const [direKillScores, setDireKillScores] = useState();
   const [time, setTime] = useState();
-  const { gameState } = useGameState({ selectedMatchId });
   const mountedRef = useCleanUp();
 
   useEffect(() => {
-    if (!matches.length) return;
-
-    const selectedMatch = matches.find(
-      (match) => match.match_id === selectedMatchId,
-    );
+    if (!currentMatch) return;
 
     const {
-      scoreboard,
-      radiant_team: {
-        team_name: rTeamName,
+      radiantTeam: {
+        name: rTeamName,
       } = {},
-      dire_team: {
-        team_name: dTeamName,
+      direTeam: {
+        name: dTeamName,
       } = {},
-    } = selectedMatch || {};
-    const { radiant, dire, duration } = scoreboard || {};
-    const { players: rPlayers } = radiant || {};
-    const { players: dPlayers } = dire || {};
+      players,
+      gameTime,
+      radiantScore,
+      direScore,
+    } = currentMatch || {};
 
-    if (!rPlayers || !dPlayers) {
-      return;
-    }
-
-    const rNetworthSum = rPlayers.reduce(
-      (acc, { net_worth: netWorth }) => acc + netWorth,
+    const rNetworthSum = players.filter((p) => p.isRadiant).reduce(
+      (acc, { networth }) => acc + networth,
       0,
     );
-    const dNetworthSum = dPlayers.reduce(
-      (acc, { net_worth: netWorth }) => acc + netWorth,
+    const dNetworthSum = players.filter((p) => !p.isRadiant).reduce(
+      (acc, { networth }) => acc + networth,
       0,
     );
 
-    const rKillScores = rPlayers.reduce(
-      (acc, { kills }) => acc + kills,
-      0,
-    );
-
-    const dKillScores = dPlayers.reduce(
-      (acc, { kills }) => acc + kills,
-      0,
-    );
-
-    const minutes = String(Math.floor(duration / 60)).padStart(2, "0");
-    const seconds = String(parseInt(duration % 60, 10)).padStart(2, "0");
+    const minutes = String(Math.floor(gameTime / 60)).padStart(2, "0");
+    const seconds = String(parseInt(gameTime % 60, 10)).padStart(2, "0");
 
     if (mountedRef.current) {
       setTime(`${minutes}:${seconds}`);
       setRadiantTeamName(rTeamName.trim());
       setDireTeamName(dTeamName.trim());
-      setRadiantKillScores(rKillScores);
-      setDireKillScores(dKillScores);
+      setRadiantKillScores(radiantScore);
+      setDireKillScores(direScore);
       setRadiantNetworthSum(rNetworthSum);
       setDireNetworthSum(dNetworthSum);
     }
-  }, [selectedMatchId, matches]);
+  }, [currentMatch]);
 
   return (
     <Text>
@@ -96,7 +75,7 @@ export default function Scoreboard({ selectedMatchId } = {}) {
       ) : <Text>{" ".repeat(5)}</Text>}
       {" "}
       |
-      {gameState === GAME_STATE.IN_GAME && time}
+      {currentMatch?.gameState === GAME_STATE.GAME_IN_PROGRESS && time}
       |
       {" "}
       {direNetworthSum > radiantNetworthSum ? (
